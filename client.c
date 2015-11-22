bc5794fd9f1c14637908dd8641d266b4c70f04ac
Eflamm <eflamm@Eflamm-portable.lan>
2015-11-22 23:53:42 +0100

    thread, envoi continu de messages

---
+++ client.c
@@ -0,0 +1,180 @@
+/*-----------------------------------------------------------
+Client a lancer apres le serveur avec la commande :
+client <adresse-serveur> <message-a-transmettre>
+------------------------------------------------------------*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <linux/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+
+typedef struct sockaddr 	sockaddr;
+typedef struct sockaddr_in 	sockaddr_in;
+typedef struct hostent 		hostent;
+typedef struct servent 		servent;
+
+int main(int argc, char **argv) {
+  
+    int     		socket_descriptor, 	/* descripteur de socket */
+		    		longueur; 		    /* longueur d'un buffer utilisé */
+    sockaddr_in 	adresse_locale; 	/* adresse de socket local */
+    hostent *		ptr_host; 		    /* info sur une machine hote */
+    servent *		ptr_service; 		/* info sur service */
+    char 			buffer[256];
+    char *			prog; 			    /* nom du programme */
+    char *			host; 			    /* nom de la machine distante */
+    char  			mesg[256]; 			/* message envoyé */
+
+    char			pseudo[50];
+    
+    //renvoie une erreur s'il n'y a pas assez d'aguments
+    /*if (argc != 3) {
+		perror("usage : client <adresse-serveur> <message-a-transmettre>");
+		exit(1);
+    }*/
+    if (argc != 2) {
+		perror("usage : client <adresse-serveur>");
+		exit(1);
+    }
+   
+    prog = argv[0];
+    host = argv[1];
+    //mesg = argv[2];
+    
+    printf("nom de l'executable : %s \n", prog);
+    printf("adresse du serveur  : %s \n", host);
+    //printf("message envoye      : %s \n", mesg);
+    
+    //renvoie une erreur, si pas de correspondance pour l'hôte
+    if ((ptr_host = gethostbyname(host)) == NULL) {
+		perror("erreur : impossible de trouver le serveur a partir de son adresse.");
+		exit(1);
+    }
+    
+    /* copie caractere par caractere des infos de ptr_host vers adresse_locale */
+    bcopy((char*)ptr_host->h_addr, (char*)&adresse_locale.sin_addr, ptr_host->h_length);
+    adresse_locale.sin_family = AF_INET; /* ou ptr_host->h_addrtype; */
+    
+    /* 2 facons de definir le service que l'on va utiliser a distance */
+    /* (commenter l'une ou l'autre des solutions) */
+    
+    /*-----------------------------------------------------------*/
+    /* SOLUTION 1 : utiliser un service existant, par ex. "irc" */
+    /*
+    if ((ptr_service = getservbyname("irc","tcp")) == NULL) {
+	perror("erreur : impossible de recuperer le numero de port du service desire.");
+	exit(1);
+    }
+    adresse_locale.sin_port = htons(ptr_service->s_port);
+    */
+    /*-----------------------------------------------------------*/
+    
+    /*-----------------------------------------------------------*/
+ 
+
+
+
+
+
+
+
+
+
+    /*________________________________________________________*/
+    /*                                                        */
+    /*-----------------CONNEXION AU SERVEUR-------------------*/
+    /*________________________________________________________*/
+
+    /* SOLUTION 2 : utiliser un nouveau numero de port */
+    adresse_locale.sin_port = htons(5000);
+    /*-----------------------------------------------------------*/
+    printf("numero de port pour la connexion au serveur : %d \n", ntohs(adresse_locale.sin_port)); 
+    /* creation de la socket */
+    if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("erreur : impossible de creer la socket de connexion avec le serveur.");
+		exit(1);
+    }   
+    /* tentative de connexion au serveur dont les infos sont dans adresse_locale */
+    if ((connect(socket_descriptor, (sockaddr*)(&adresse_locale), sizeof(adresse_locale))) < 0) {
+		perror("erreur : impossible de se connecter au serveur.");
+		exit(1);
+    }   
+    printf("connexion etablie avec le serveur. \n\n\n");
+
+
+    /*________________________________________________________*/
+    /*--------------------------------------------------------*/
+
+
+
+
+
+
+
+    /*________________________________________________________*/
+    /*                                                        */
+    /*-----------------DIALOGUE DANS LE CHAT------------------*/
+    /*________________________________________________________*/
+
+    
+	printf("Bienvenue dans le chat 		serveur: %s\n", host);
+	printf("__________________________________________________\n\n");
+    
+    //Le client doit entrer son pseudo
+    printf("Entrez cotre pseudo: \n");
+    fgets(pseudo, sizeof pseudo, stdin);
+    // scanf("%s", pseudo);
+    if ((write(socket_descriptor, pseudo, strlen(pseudo))) < 0) {
+            perror("erreur : impossible d'ecrire le message destine au serveur.");
+            exit(1);
+    }
+
+    //Le client peut désormais envoyer des messages au serveur
+    //Il pourra quitter en écrivant la commande "/q" 
+	while(strcmp(mesg,"/q")!=0){
+		// printf("début\n");
+        
+        fgets(mesg, sizeof(mesg), stdin);
+        //avec fgets() le caractere de saut de ligne '\n' est rajouté,
+        //il faut s'en débarrassé pour faire fonctionner strcmp
+        mesg[strcspn(mesg, "\n")] = '\0'; //enlève le caractère de saut de ligne 
+
+		//Le client envoie le message
+		if ((write(socket_descriptor, mesg, strlen(mesg))) < 0) {
+			perror("erreur : impossible d'ecrire le message destine au serveur.");
+			exit(1);
+    	}
+    	// printf("milieu \n");
+    	//Le client attend de recevoir le message pour retourner l'ACK
+    	// while((longueur = read(socket_descriptor, buffer, sizeof(buffer))) > 0) {
+			longueur = read(socket_descriptor, buffer, (int)sizeof(buffer));
+			printf("reponse du serveur : \n");
+			write(1,buffer,longueur);
+    	// }
+
+    	// printf("fin\n");
+	}
+	printf("Vous quittez le chat.\n");
+    printf("__________________________________________________\n\n");
+
+    /*________________________________________________________*/
+    /*--------------------------------------------------------*/
+    
+
+
+
+
+
+
+
+    printf("\nfin de la reception.\n");
+    
+    close(socket_descriptor);
+    
+    printf("connexion avec le serveur fermee, fin du programme.\n");
+    
+
+    exit(0);
+    
+}
---
+++ server.c
@@ -0,0 +1,262 @@
+/*----------------------------------------------
+Serveur à lancer avant le client
+------------------------------------------------*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <linux/types.h> 	/* pour les sockets */
+#include <sys/socket.h>
+#include <netdb.h> 		/* pour hostent, servent */
+#include <string.h> 		/* pour bcopy, ... */  
+#include <pthread.h> 
+#define TAILLE_MAX_NOM 256
+#define NB_CLIENTS_MAX 42
+
+typedef struct sockaddr sockaddr;
+typedef struct sockaddr_in sockaddr_in;
+typedef struct hostent hostent;
+typedef struct servent servent;
+
+//Structure qui définit un client du serveur
+typedef struct{
+    int sock;               /*socket du client*/
+    char pseudo[50];        /*pseudo du client*/
+    pthread_t thread;       /*thread du serveur auquel est affecté le client*/
+    int connected;         /*booléen indiquant si le client est connecté ou non*/
+} Client;
+
+
+
+
+
+
+
+/*------------------------------------------------------*/
+//Renvoie le message reçu par le client
+void renvoi (int sock) {
+
+    char buffer[256];
+    char *answer = malloc (sizeof (*answer) * 256);
+    int longueur;
+   
+    if ((longueur = read(sock, buffer, sizeof(buffer))) <= 0) 
+    	return;
+    
+    printf("message lu : %s \n", buffer);
+    
+    answer[0] = 'R';
+    answer[1] = 'E';
+    answer[2] = '#';
+    answer = strcat(answer, buffer);
+    answer[longueur+4] ='\0';
+    
+    printf("message apres traitement : %s \n", answer);
+    
+    printf("renvoi du message traite.\n");
+
+    /* mise en attente du programme pour simuler un delai de transmission */
+    sleep(3);
+    
+    write(sock,answer,strlen(answer)+1);
+    free(answer);
+    printf("message envoye. \n");
+
+    return;
+    
+}
+
+
+    /*______________________________________________________________________________________*/
+    /*                                                                                      */
+    /*--------------------------TRAITEMENT DES COMMANDES------------------------------------*/
+    /*______________________________________________________________________________________*/
+
+//Traite la commande reçue par le client
+static void * commande (void * c){
+    Client * client = (Client *) c;
+	char buffer[256];
+	char *answer = malloc (sizeof (*answer) * 256);
+	int longueur;
+
+    //Si le client n'a pas de pseudo
+    printf("Test taille pseudo %i\n", strlen((*client).pseudo)==0);
+    printf("el pseudo %s\n", (*client).pseudo);
+    while(strlen((*client).pseudo)<=1){
+        longueur = read((*client).sock, buffer, sizeof(buffer));
+        buffer[longueur]='\0'; 
+        printf("Le pseudo %s\n", buffer);
+        strcpy((*client).pseudo, buffer);
+        write(1,buffer,longueur);
+    }
+	
+	
+    while(1){
+
+    	longueur = read((*client).sock, buffer, sizeof(buffer));
+
+    	//Permet de vider le buffer des ancinnes données
+        buffer[longueur]='\0';    	// explicit null termination: updated based on comments
+        // printf("%s\n",buffer); 		// print the current receive buffer with a newline
+        // fflush(stdout);         	// make sure everything makes it to the output
+        // buffer[0]='\0';
+
+    	printf("message lu : %s \n", buffer);
+
+    	sleep(3);
+    	//Si le buffer contient la commande de sortie
+    	if(strcmp(buffer,"/q")==0){
+            printf("Le client quitte le chat.\n");
+            answer = "Au revoir!\n";
+            write((*client).sock,answer,strlen(answer)+1);
+            pthread_exit(NULL);
+    	}
+    	else if(longueur > 0){
+    		printf("renvoi\n");
+    		answer = "Roger 5 sur 5 \n";
+    		write((*client).sock,answer,strlen(answer)+1);
+    	}
+
+
+
+    }
+
+    return;
+
+}
+
+    /*______________________________________________________________________________________*/
+    /*______________________________________________________________________________________*/
+    /*--------------------------------------------------------------------------------------*/ 
+
+
+/*------------------------------------------------------*/
+
+
+
+
+
+
+/*------------------------------------------------------*/
+main(int argc, char **argv) {
+  
+    int             socket_descriptor, 			/* descripteur de socket */
+	               	nouv_socket_descriptor, 	/* [nouveau] descripteur de socket */
+			     	longueur_adresse_courante; 	/* longueur d'adresse courante d'un client */
+    sockaddr_in 	adresse_locale, 			/* structure d'adresse locale*/
+			        adresse_client_courant; 	/* adresse client courant */
+    hostent*		ptr_hote; 					/* les infos recuperees sur la machine hote */
+    servent*		ptr_service; 				/* les infos recuperees sur le service de la machine */
+    char 			machine[TAILLE_MAX_NOM+1]; 	/* nom de la machine locale */
+
+
+    Client arrClient[NB_CLIENTS_MAX];           /*Tableau contenant tous les clients*/
+    int nb_client = 0;                          /*Nombre de clients connectés au serveur*/
+
+
+
+
+
+    gethostname(machine,TAILLE_MAX_NOM);		/* recuperation du nom de la machine */
+    
+    /* recuperation de la structure d'adresse en utilisant le nom */
+    if ((ptr_hote = gethostbyname(machine)) == NULL) {
+		perror("erreur : impossible de trouver le serveur a partir de son nom.");
+		exit(1);
+    }
+    
+    /* initialisation de la structure adresse_locale avec les infos recuperees */			
+    
+    /* copie de ptr_hote vers adresse_locale */
+    bcopy((char*)ptr_hote->h_addr, (char*)&adresse_locale.sin_addr, ptr_hote->h_length);
+    adresse_locale.sin_family		= ptr_hote->h_addrtype; 	/* ou AF_INET */
+    adresse_locale.sin_addr.s_addr	= INADDR_ANY; 			/* ou AF_INET */
+
+    /* 2 facons de definir le service que l'on va utiliser a distance */
+    /* (commenter l'une ou l'autre des solutions) */
+    
+    /*-----------------------------------------------------------*/
+    /* SOLUTION 1 : utiliser un service existant, par ex. "irc" */
+    /*
+    if ((ptr_service = getservbyname("irc","tcp")) == NULL) {
+		perror("erreur : impossible de recuperer le numero de port du service desire.");
+		exit(1);
+    }
+    adresse_locale.sin_port = htons(ptr_service->s_port);
+    */
+    /*-----------------------------------------------------------*/
+    /* SOLUTION 2 : utiliser un nouveau numero de port */
+    adresse_locale.sin_port = htons(5000);
+    /*-----------------------------------------------------------*/
+    
+    printf("numero de port pour la connexion au serveur : %d \n", 
+		   ntohs(adresse_locale.sin_port) /*ntohs(ptr_service->s_port)*/);
+    
+    /* creation de la socket */
+    if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("erreur : impossible de creer la socket de connexion avec le client.");
+		exit(1);
+    }
+
+    /* association du socket socket_descriptor à la structure d'adresse adresse_locale */
+    if ((bind(socket_descriptor, (sockaddr*)(&adresse_locale), sizeof(adresse_locale))) < 0) {
+		perror("erreur : impossible de lier la socket a l'adresse de connexion.");
+		exit(1);
+    }
+    
+
+
+
+
+
+
+
+    /*______________________________________________________________________________________*/
+    /*                                                                                      */
+    /*---------------------------------SERVEUR EN ECOUTE------------------------------------*/
+    /*______________________________________________________________________________________*/
+
+
+    /* initialisation de la file d'ecoute */
+    listen(socket_descriptor,5);
+
+    /* attente des connexions et traitement des donnees recues */
+    while(1) {
+    
+		longueur_adresse_courante = sizeof(adresse_client_courant);
+		
+		/* adresse_client_courant sera renseigné par accept via les infos du connect */
+        /*on check s'il y a assez de place sur le serveur*/
+		if (nb_client >= NB_CLIENTS_MAX) {
+					perror("erreur : le serveur est saturé");
+					exit(1);
+		}
+
+
+
+        else{
+            //si le client n'est pas encore connecté
+            if(arrClient[nb_client].connected == 0){
+                if ((nouv_socket_descriptor = accept(socket_descriptor,(sockaddr*)(&adresse_client_courant),&longueur_adresse_courante))< 0) {
+                    perror("erreur : impossible d'accepter la connexion avec le client.");
+                    exit(1);
+                }
+                //création d'un thread pour le client
+                else{
+                    arrClient[nb_client].connected = 1;
+                    arrClient[nb_client].pseudo[0] = '\0';
+                    arrClient[nb_client].sock = nouv_socket_descriptor;
+                    pthread_create(&arrClient[nb_client].thread, NULL, commande, &arrClient[nb_client]);
+                    nb_client++;    //on incrémente le nombre de clients connectés sur le serveur
+                }                
+            }
+        }
+
+
+
+    }
+
+    /*______________________________________________________________________________________*/
+    /*______________________________________________________________________________________*/
+    /*--------------------------------------------------------------------------------------*/ 
+
+    
+}
